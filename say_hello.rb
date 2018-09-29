d9f8d5475fd9fb60f5e49f0a95cb704e1680e14a
joi moore <joimoore0@gmail.com>
2018-09-29 16:26:53 +0000

    automatically backed up by learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,16 +1,2 @@
-# Build your say_hello method here
-def hello_world
-  puts "hello world"
-end
-
 def say_hello(name)
-  puts name
-end
-
-hello_world
-say_hello "Joy"
-say_hello "world"
-def_hello world
-puts say_hello (Gabriela")
-puts name
-end
+  
